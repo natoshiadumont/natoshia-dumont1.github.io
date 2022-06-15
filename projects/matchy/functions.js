5640fed1b7f1094a11f2a877598423e92aeac9b6
Natoshia Dumont <natoshia.dumont@gmail.com>
2022-06-15 01:09:06 +0000

    matchy project complete!

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -13,23 +13,73 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+//create function search that takes in a parameter of an array, of animal objects, and a string
+function search(array, string) {
+    //use for loop to iterate through array
+    for (let i = 0; i < array.length; i++){
+        //determine if string and array.name are equal, use toUpperCase method to make case insensitive
+        if(array[i].name === string){
+            //if true, return entire element's object array[i]
+            return array[i];
+        }
+    }
+    //return null (if no element's names match string)
+    return null;
+}
+    
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//make function replace with parameters: animals, name, replacement
+function replace(animals, name, replacement){
+    //use for loop to iterate through animals array
+    for (let i = 0; i < animals.length; i++) {
+     //determine if animals[i].name is equal to string
+     if (animals[i].name === name) {
+        //if condition is true, replace current element with replacement argument
+        animals[i] = replacement;
+     } 
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+//make function remove with parameters: animals, name
+function remove(animals, name){
+    //use for loop to iterate through animal array
+    for (let i = 0; i < animals.length; i++){
+        //determine if animala[i].name === name
+        if(animals[i].name === name){
+            //if condition returns true, use splice to remove current element
+            animals.splice(i, 1);
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+
+//create add function with 2 parameters: animals(animals array of objects) and animal(new animal object)
+function add(animals, animal) {
+    //determine if animal object has a name and species property with lengths greater than zero
+    if (animal.name.length > 0 && animal.species.length > 0) {
+        for (let i = 0; i < animals.length; i++) {
+            //determine if animal object name property is a match; if so, do nothing
+            if(animals[i].name === animal.name) {
+                return animals;
+            }    
+        }
+        //push animal objectinto animals array
+        animals.push(animal);
+    }
+            
+}
 
 
 
